8ee1ad933f702f9a263a6c761bf54469ef59c493
D_Zucho <danny.nico.dev@outlook.com>
2023-12-18 11:20:42 -0800

    add delete todo functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -10,6 +11,15 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
+  const [edit, setEdit] = useState<boolean>(false)
+  const [todoString, setTodoString] = useState<string>(todo.todo)
+
+  //* Handle edit todo
+  // const editTodo = (id: number) => {
+
+  // }
+
+  //* Handle complete todo
   const handleComplete = (id: number) => {
     setTodoList(
       todoList.map((todo) => {
@@ -20,31 +30,44 @@ const SingleTodo: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
       })
     )
   }
+
+  //* Handle delete todo
+  const handleDelete = (id: number) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id))
+  }
   return (
     <form
       id='single-todo'
       className={classNames(
         'w-full md:w-[45%] xl:w-[30%] p-5 rounded-md flex items-center justify-between transition-all ease-in-out duration-500',
-        !todo.completed ? 'bg-green-500 ' : 'bg-yellow-400'
+        todo.completed ? 'bg-green-500 ' : 'bg-yellow-400'
       )}
     >
       {todo.completed ? (
-        <span className='p-1.5 border-none text-xl flex-1 focus:outline-none'>
-          {todo.todo}
-        </span>
-      ) : (
         <s className='p-1.5 border-none text-xl flex-1 focus:outline-none'>
           {todo.todo}
         </s>
+      ) : (
+        <span className='p-1.5 border-none text-xl flex-1 focus:outline-none'>
+          {todo.todo}
+        </span>
       )}
 
-      {/* Icons Start */}
+      {/*******************
+       *   Icons Start
+       ****************** */}
       <div id='icons' className='flex'>
         <span className='todo-icon'>
-          <AiFillEdit />
+          <AiFillEdit
+            onClick={() => {
+              if (!edit && !todo.completed) {
+                setEdit(!edit)
+              }
+            }}
+          />
         </span>
         <span className='todo-icon'>
-          <AiFillDelete />
+          <AiFillDelete onClick={() => handleDelete(todo.id as number)} />
         </span>
         <span className='todo-icon'>
           <MdDone onClick={() => handleComplete(todo.id as number)} />
