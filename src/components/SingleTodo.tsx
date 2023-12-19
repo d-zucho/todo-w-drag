a69540df4a7e7f10896a35d8582ea3c360986ef1
D_Zucho <danny.nico.dev@outlook.com>
2023-12-18 18:47:34 -0800

    add edit and update final functionality and simple styling

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -14,10 +14,20 @@ const SingleTodo: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
   const [edit, setEdit] = useState<boolean>(false)
   const [todoString, setTodoString] = useState<string>(todo.todo)
 
-  //* Handle edit todo
-  // const editTodo = (id: number) => {
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  // }
+  //* Handle edit todo
+  const handleSubmitEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault()
+    // if id matches, update todo to what is in todoString
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, todo: todoString } : todo
+      )
+    )
+    // set editTodo back to false
+    setEdit(false)
+  }
 
   //* Handle complete todo
   const handleComplete = (id: number) => {
@@ -35,6 +45,11 @@ const SingleTodo: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
   const handleDelete = (id: number) => {
     setTodoList(todoList.filter((todo) => todo.id !== id))
   }
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+
   return (
     <form
       id='single-todo'
@@ -42,8 +57,22 @@ const SingleTodo: React.FC<Props> = ({ todo, todoList, setTodoList }) => {
         'w-full md:w-[45%] xl:w-[30%] p-5 rounded-md flex items-center justify-between transition-all ease-in-out duration-500',
         todo.completed ? 'bg-green-500 ' : 'bg-yellow-400'
       )}
+      onSubmit={(e) => handleSubmitEdit(e, todo.id as number)}
     >
-      {todo.completed ? (
+      {/*******************
+       *   Edit Start
+       ****************** */}
+      {edit ? (
+        <input
+          ref={inputRef}
+          type='text'
+          className='p-1.5 border-none text-xl flex-1 focus:outline-none rounded-lg'
+          value={todoString}
+          onChange={(e) => {
+            setTodoString(e.target.value)
+          }}
+        />
+      ) : todo.completed ? (
         <s className='p-1.5 border-none text-xl flex-1 focus:outline-none'>
           {todo.todo}
         </s>
